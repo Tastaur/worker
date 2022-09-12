bac5c85ad887835ee73a4962b8a403a466621536
Соболев Марк <mark.sobolev@cosysoft.ru>
2022-09-12 09:33:40 +0400

    add examples

--- example/second.js
+++ example/second.js
@@ -9,7 +9,7 @@ const start = performance.now()
 // process.env.UV_THREADPOOL_SIZE = 8
 
 console.log('start')
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 1; i++) {
     pbkdf2('test', 'javascript', 1000000, 64, 'sha512', (err, data) => {
         console.log(`${i + 1} ${performance.now() - start}`)
     })
